94be87fe538b131a5c508a1cc2d20457ed77f746
dsantoro <davide.santoro@soprasteria.com>
2018-06-19 15:28:18 +0200

    sdasdas

--- index.js
+++ index.js
@@ -66,9 +66,8 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function (req, res) {
-	var speech =
-		req.body.result &&
-		req.body.result.parameters &&
+	var speech;
+	speech = req.body.result && req.body.result.parameters &&
 		req.body.result.parameters.echoText
 			? req.body.result.parameters.echoText
 			: "Seems like some problem. Speak again.";
